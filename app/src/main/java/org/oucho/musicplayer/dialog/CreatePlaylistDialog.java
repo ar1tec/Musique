7c7c9054e124ca1759b1d123341f7483cec89c67
toto <toto@dev.null.com>
2016-06-29 22:52:03 +0200

    auto show keyboard on create playlist

--- app/src/main/java/org/oucho/musicplayer/dialog/CreatePlaylistDialog.java
+++ app/src/main/java/org/oucho/musicplayer/dialog/CreatePlaylistDialog.java
@@ -1,12 +1,15 @@
 package org.oucho.musicplayer.dialog;
 
 import android.app.Dialog;
+import android.content.Context;
 import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.v4.app.DialogFragment;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 
@@ -27,8 +30,12 @@ public class CreatePlaylistDialog extends DialogFragment {
     @NonNull
     @Override
     public Dialog onCreateDialog(Bundle savedInstanceState) {
+
+
         final View layout = LayoutInflater.from(getActivity()).inflate(R.layout.create_playlist_dialog,
                 new LinearLayout(getActivity()), false);
+
+
 
         android.app.AlertDialog.Builder builder = new android.app.AlertDialog.Builder(getActivity())
                 .setTitle(R.string.create_playlist)
@@ -53,6 +60,9 @@ public class CreatePlaylistDialog extends DialogFragment {
                                 // This constructor is intentionally empty, pourquoi ? parce que !
                             }
                         });
+
+        InputMethodManager imm = (InputMethodManager) getContext().getSystemService(Context.INPUT_METHOD_SERVICE);
+        imm.toggleSoftInput(InputMethodManager.SHOW_FORCED,0);
 
         return builder.create();
     }
